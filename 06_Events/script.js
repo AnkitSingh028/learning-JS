9fe486d516618efb7a0ca37490ace7a52c8701a6
Ankit Singh <guypov7@gmail.com>
2026-02-13 08:32:25 +0000

    Learning the concepts of Events in JS

---
+++ 06_Events/script.js
@@ -0,0 +1,24 @@
+//Event - The change in the state of an object is known as Event//
+/*TYPES
+1.Mouse events(clicks,double click etc.)
+2.Keyboard events(keypress,keyup,keydown)
+3.print event & many more*/
+
+// /*Event Handling in JS
+node.event = () => {
+//handle here
+}
+
+
+/*EXAMPLE*/
+/*btn.onclick = ()=>{
+    console.log("btn was clicked");
+    } */
+
+    
+    
+    /*EVENT LISTENER*/
+
+    /*node.addEventListener(event, callback)
+    
+    node.removeEventListener(event, callback)*/
